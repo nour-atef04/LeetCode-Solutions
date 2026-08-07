d787d92dfd5b9dabba6323f5e6be1df95829a422
nour-atef04 <138708947+nour-atef04@users.noreply.github.com>
2026-08-07 12:18:43 +0300

    Time: 74 ms (39.91%) | Memory: 73.7 MB (49.96%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -3,19 +3,19 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<pair<int, pair<int, int>>> maxHeap;
 
-        for(const auto& point : points){
-            int diff = point[0]*point[0] + point[1]*point[1];
-            maxHeap.push({diff, {point[0], point[1]}});
-            if (maxHeap.size() > k){
-                maxHeap.pop();
-            }
+        for(vector<int> point : points){
+            int dist = point[0]*point[0] + point[1]*point[1];
+            maxHeap.push({dist, {point[0], point[1]}});
+            if(maxHeap.size() > k) maxHeap.pop();
         }
+
         vector<vector<int>> res;
+
         while(!maxHeap.empty()){
-            auto [dist, point] = maxHeap.top();
-            res.push_back({point.first, point.second});
+            res.push_back({maxHeap.top().second.first, maxHeap.top().second.second});
             maxHeap.pop();
         }
+
         return res;
     }
 };
